36956a385d074ef8f573d99fe5db8f5d0623fd94
nvcleemp <nico.vancleemput@gmail.com>
2019-01-31 20:41:55 +0100

    Added support for graphs with sinks

--- tools/adjlist2wcf.py
+++ tools/adjlist2wcf.py
@@ -6,8 +6,9 @@ def writeGraph(lines, zeroBased):
     sys.stdout.write('{:c}'.format(len(lines)))
     for line in lines:
         _,line = line.split(':')
-        for neighbour in [int(n) + (1 if zeroBased else 0) for n in line.split(',')]:
-            sys.stdout.write('{:c}'.format(neighbour))
+        if line.strip():
+            for neighbour in [int(n) + (1 if zeroBased else 0) for n in line.split(',')]:
+                sys.stdout.write('{:c}'.format(neighbour))
         sys.stdout.write('{:c}'.format(0))
         
 #parse command-line arguments
